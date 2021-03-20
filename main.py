3064921cfb4ad5a33a10b70958a9f6496fa8b24f
NorbertSzczepanski <74811587+NorbertSzczepanski@users.noreply.github.com>
2021-03-20 16:20:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,52 @@ import sys as system
 
 print('Zadanie1')
 zdanie = input('Napisz zdanie: ')
-print(zdanie.count('a'))
+ilosca = zdanie.count('a')
+print('Litera "a" wystąpiła %d razy w zdaniu.' % ilosca)
 
-print('Zadanie2')
-system.stdout.write('Podaj 3 liczby całkowite: ')
-a = system.stdin.readline()
-b = system.stdin.readline()
-c = system.stdin.readline()
-a = int(a)
-b = int(b)
-c = int(c)
-system.stdout.write('Wynik (a^b)+c to: ' + str((a ** b) + c))
+print('\nZadanie2')
+system.stdout.write('Podaj liczbę całkowitą "a": ')
+a = int(system.stdin.readline())
+system.stdout.write('Podaj liczbę całkowitą "b": ')
+b = int(system.stdin.readline())
+system.stdout.write('Podaj liczbę całkowitą "c": ')
+c = int(system.stdin.readline())
+wzor = a ** b + c
+system.stdout.write('Wynik działania (a^b)+c to: %d' % wzor)
 
-print('\nZadanie3')
-system.stdout.write('Podaj 3 liczby całkowite: ')
-d = system.stdin.readline()
-e = system.stdin.readline()
-f = system.stdin.readline()
-d = int(d)
-e = int(e)
-f = int(f)
+print('\n\nZadanie3')
+system.stdout.write('Podaj liczbę całkowitą "a": ')
+a = int(system.stdin.readline())
+system.stdout.write('Podaj liczbę całkowitą "b": ')
+b = int(system.stdin.readline())
+system.stdout.write('Podaj liczbę całkowitą "c": ')
+c = int(system.stdin.readline())
 
-if d >= e:
-    if d >= f:
-        najwiekszaliczba = d
-        print('Najwiekszą liczbą jest: ' + str(najwiekszaliczba))
-if e >= d:
-    if e >= f:
-        najwiekszaliczba = e
-        print('Największą liczbą jest: ' + str(najwiekszaliczba))
-if f >= d:
-    if f >= e:
-        najwiekszaliczba = f
-        print('Największą liczbą jest: ' + str(najwiekszaliczba))
+najwiekszaliczba = a
+if najwiekszaliczba < b:
+    najwiekszaliczba = b
+    if najwiekszaliczba < c:
+        najwiekszaliczba = c
+print('Największa liczba to %d' % najwiekszaliczba)
+
+print('\nZadanie4')
+liczby = [4, 5.16, 2, 98, 4.15, 8.9, 12, 54, 100.5, 67.2, 14.4, 82]
+print(liczby)
+
+counter = 0
+for element in liczby:
+    liczby[counter] = liczby[counter] ** 2
+    counter += 1
+print('Liczby z listy "liczby" podniesione do kwadratu: ' + str(liczby))
+
+print('\nZadanie5')
+parzyste = []
+
+counter = 0
+while counter != 10:
+    liczba = int(input('Podaj liczbę całkowitą: '))
+    if liczba % 2 == 0:
+        parzyste.append(liczba)
+    counter += 1
+else:
+    print('Liczby parzyste: ' + str(parzyste))
